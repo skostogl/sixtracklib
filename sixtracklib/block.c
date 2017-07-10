23e59e94a8d34e4922edbcb5ad0ab540928264ca
Omar Awile <omar.awile@cern.ch>
2017-07-10 13:50:15 +0200

    moved element selection outside of single_track function
    and annoted code

--- sixtracklib/block.c
+++ sixtracklib/block.c
@@ -39,15 +39,14 @@ CLGLOBAL uint64_t *Block_get_elemids(CLGLOBAL value_t *data, size_t elemid ) {
 
 int track_single(CLGLOBAL value_t *data,
                  CLGLOBAL Particle *particles,
-                 CLGLOBAL uint64_t *elemids,
-                 Particle *p, uint64_t i_elem, uint64_t i_part,
+                 uint64_t elemid,
+                 Particle *p, uint64_t i_part,
                  uint64_t elembyelemoff, uint64_t turnbyturnoff){
    //CLGLOBAL Particle* p = &particles[i_part];
    CLGLOBAL value_t *elem;
-   uint64_t elemid;
    if (p->state >= 0 ) {
-       elemid=elemids[i_elem];
-       if ( (turnbyturnoff>0) && (i_elem==0) ){
+       // diagnostics, will be always 0 if diagnostics disabled in Block_track
+       if (turnbyturnoff>0) {
          uint64_t dataoff=turnbyturnoff+sizeof(Particle)/8 * i_part;
          for (int i_attr=0;i_attr<sizeof(Particle)/8;i_attr++) {
             data[dataoff + i_attr] =
@@ -58,15 +57,19 @@ int track_single(CLGLOBAL value_t *data,
        elem=data+elemid+1; //Data starts after typeid
 //       _DP("Block_track: elemid=%zu typedid=%u\n",elemid,typeid);
        switch (typeid) {
+         // 50% 
            case DriftID:
                 Drift_track(p, (CLGLOBAL Drift*) elem);
            break;
+           // 30%
            case MultipoleID:
                 Multipole_track(p, (CLGLOBAL Multipole*) elem);
            break;
+           // 0.1%
            case CavityID:
                 Cavity_track(p, (CLGLOBAL Cavity*) elem);
            break;
+           // 0 - 50%
            case AlignID:
                 Align_track(p, (CLGLOBAL Align*) elem);
            break;
@@ -77,6 +80,7 @@ int track_single(CLGLOBAL value_t *data,
                 DriftExact_track(p, (CLGLOBAL DriftExact*) elem);
            break;
        }
+       // diagnostics, will be always 0 if diagnostics disabled in Block_track
        if (elembyelemoff>0){
          uint64_t dataoff=elembyelemoff+sizeof(Particle)/8 * i_part;
          for (int i_attr=0;i_attr<sizeof(Particle)/8;i_attr++) {
@@ -129,7 +133,7 @@ CLKERNEL void Block_track(
 #include <stdio.h>
 
 
-int Block_track(value_t *data, Beam *beam,
+int Block_track(value_t *data, Beam *restrict beam,
                 uint64_t blockid, uint64_t nturn,
                 uint64_t elembyelemid, uint64_t turnbyturnid){
    uint64_t nelem    = Block_get_nelen(data, blockid);
@@ -137,24 +141,31 @@ int Block_track(value_t *data, Beam *beam,
    uint64_t npart=beam->npart;
    uint64_t elembyelemoff=0;
    uint64_t turnbyturnoff=0;
+   // for each revolution around accelerator ...
    for (int i_turn=0; i_turn< nturn; i_turn++) {
+     // for each accelerator element ...
      for (int i_elem=0; i_elem< nelem; i_elem++) {
+       uint64_t elemid=elemids[i_elem];
+//#pragma omp parallel for
+       // for each particle ...
        for (uint64_t i_part=0; i_part < npart; i_part++){
-          Particle pp=particles[i_part];
+          Particle pp=beam->particles[i_part];
+          // diagnostics 
           if (elembyelemid>0){
             elembyelemoff=elembyelemid +
                          sizeof(Particle)/8 * npart * i_turn +
                          sizeof(Particle)/8 * npart * nturn  * i_elem ;
 //            printf("cpu %lu \n",elembyelemoff);
           }
+          // diagnostics
           if (turnbyturnid>0){
             turnbyturnoff=turnbyturnid +
                          sizeof(Particle)/8 * npart * i_turn;
 //            printf("%lu \n",turnbyturnoff);
           }
-          track_single(data, beam->particles, elemids,
-                       &pp, i_elem, i_part, elembyelemoff, turnbyturnoff);
-          particles[i_part]=pp;
+          track_single(data, beam->particles, elemid,
+                       &pp, i_part, elembyelemoff, turnbyturnoff);
+          beam->particles[i_part]=pp;
        }
      }
      for (uint64_t i_part=0; i_part < npart; i_part++){
